b153804a1c91e5ffe18b4eb7cb6e085e0366ac3c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-27 00:10:57 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Project from './pages/Project';
+import Services from './pages/Services';
 
-export default function App() {
+ const  App = () => {
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </Router>
+  );
+
 }
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ml,body{
+    font-family: 'Oswald', sans-serif;
+    font-size: 62.5%;
+
+    @media (max-width: 768px) {
+      font-size: 50%;
+    }
+
+    @media (max-width: 450px) {
+      font-size: 40%;
+    }
+
+  }
+
+  body{
+    font-size: 1.6rem
+  }
+
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
