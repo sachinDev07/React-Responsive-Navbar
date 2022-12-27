af96bc9cd4c0f8d53a449befbfd5d69a9556c2b0
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-28 01:04:38 +0500

    done navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Project from './pages/Project';
+import Product from './pages/Product';
 import Services from './pages/Services';
+import Navigation from './components/Navigation/Navigation'
 
  const  App = () => {
 
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/project' element={<Project />} />
+        <Route path='/products' element={<Product />} />
         <Route path='/services' element={<Services />} />
       </Routes>
     </Router>
