26e6445e1c17df7fe6aa9248e41cce7404587a76
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-28 01:07:12 +0500

    done Product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BigGradientText, CenterText } from '../styles/style'
+
+const Product = () => {
+  return (
+    <CenterText>
+        <BigGradientText>Products</BigGradientText>
+    </CenterText>
+  );
+}
+
+export default Product;
